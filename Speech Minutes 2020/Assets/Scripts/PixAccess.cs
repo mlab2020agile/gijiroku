914ba8fb7b57fb73e454c08d94920e4fe56446f9
YusakuMaeyama <75418006+YusakuMaeyama@users.noreply.github.com>
2021-08-02 13:29:51 +0900

    線を滑らかにした

--- Speech Minutes 2020/Assets/Scripts/PixAccess.cs
+++ Speech Minutes 2020/Assets/Scripts/PixAccess.cs
@@ -29,6 +29,8 @@ public class PixAccess : MonobitEngine.MonoBehaviour
 	float x1 = 0.0f;
 	float y1 = 0.0f;
 
+	private Vector2 _prevPosition;
+
 	public void Start()
 	{
 		Texture2D mainTexture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
@@ -193,45 +195,87 @@ public class PixAccess : MonobitEngine.MonoBehaviour
 
 		if (mode)
 		{
-			if (Input.GetMouseButtonDown(0))
-			{
-				Vector3 mousePos = Input.mousePosition;
-				float x2 = mousePos.x;
-				float y2 = mousePos.y;
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit hit;
-				if (Physics.Raycast(ray, out hit, 100.0f))
-				{
-					monobitView.RPC("Draw", MonobitTargets.All, hit.textureCoord * 256);
-				}
-				drawTexture.SetPixels(buffer);
-				drawTexture.Apply();
-				GetComponent<Renderer>().material.mainTexture = drawTexture;
-				x1 =x2;
-				y1 =y2;
-			}
 			if (Input.GetMouseButton(0))
 			{
-				Vector3 mousePos = Input.mousePosition;
-				float x2 = mousePos.x;
-				float y2 = mousePos.y;
-				/*else
-				{
-					drawTexture.moveTo(x1,y1,0);
-					drawTexture.lineTo(x2,y2,0);
-				}*/
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit hit;
-				if (Physics.Raycast(ray, out hit, 100.0f))
-				{
-					monobitView.RPC("Draw", MonobitTargets.All, hit.textureCoord * 256);
-				}
-				drawTexture.SetPixels(buffer);
-				drawTexture.Apply();
-				GetComponent<Renderer>().material.mainTexture = drawTexture;
-				x1 =x2;
-				y1 =y2;
-			}
+				//Vector3 mousePos = Input.mousePosition;
+				//float x2 = mousePos.x;
+				//float y2 = mousePos.y;
+				//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				//RaycastHit hit;
+				//if (Physics.Raycast(ray, out hit, 100.0f))
+				//{
+				//	monobitView.RPC("Draw", MonobitTargets.All, hit.textureCoord * 256);
+				//}
+				//drawTexture.SetPixels(buffer);
+				//drawTexture.Apply();
+				//GetComponent<Renderer>().material.mainTexture = drawTexture;
+				//x1 =x2;
+				//y1 =y2;
+
+				//前回値がまだないなら現在の値を前回値として扱う
+            	if (_prevPosition == Vector2.zero)
+            	{
+                	_prevPosition = Input.mousePosition;
+            	}
+
+            	//線形補間に使う入力の終点座標
+            	Vector2 endPosition = Input.mousePosition;
+            	//1フレームの線の距離
+            	float lineLength = Vector2.Distance(_prevPosition, endPosition);
+            	//線の長さに応じて変わる補間値　CeilToIntは小数点以下を切り上げ
+            	int lerpCountAdjustNum = 5;
+            	int lerpCount = Mathf.CeilToInt(lineLength / lerpCountAdjustNum);
+
+            	for (int i = 1; i <= lerpCount; i++)
+            	{
+                	//Lerpの割合値を "現在の回数/合計回数" で出す
+                	float lerpWeight = (float) i / lerpCount;
+
+                	//前回の入力座標、現在の入力座標、割合を渡して補間する座標を算出
+                	Vector3 lerpPosition = Vector2.Lerp(_prevPosition, Input.mousePosition, lerpWeight);
+
+                	Ray ray = Camera.main.ScreenPointToRay(lerpPosition);
+                	RaycastHit hit;
+                	if (Physics.Raycast(ray, out hit, 100.0f))
+                	{
+                	    monobitView.RPC("Draw", MonobitTargets.All, hit.textureCoord * 256);
+                	}
+
+                	drawTexture.SetPixels(buffer);
+                	drawTexture.Apply();
+                	GetComponent<Renderer>().material.mainTexture = drawTexture;
+            	}
+
+            	//前回の入力座標を記録
+            	_prevPosition = Input.mousePosition;
+        	}
+        	else
+        	{
+            	//前回の入力座標をリセット
+            	_prevPosition = Vector2.zero;
+        	}
+				//if (Input.GetMouseButton(0))
+				//{
+					//Vector3 mousePos = Input.mousePosition;
+					//float x2 = mousePos.x;
+					//float y2 = mousePos.y;
+					/*else
+					{
+						drawTexture.moveTo(x1,y1,0);
+						drawTexture.lineTo(x2,y2,0);
+					}*/
+					//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+					//RaycastHit hit;
+					//if (Physics.Raycast(ray, out hit, 100.0f))
+					//{
+						//monobitView.RPC("Draw", MonobitTargets.All, hit.textureCoord * 256);
+					//}
+					//drawTexture.SetPixels(buffer);
+					//drawTexture.Apply();
+					//GetComponent<Renderer>().material.mainTexture = drawTexture;
+					//x1 =x2;
+					//y1 =y2;
+				//}
 		}
 	}
 }

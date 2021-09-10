d5d8aa7a9cfd2f355416ea99c41a3c3d47acd150
Okouchi <cstk18015@g.nihon-u.ac.jp>
2021-09-10 17:05:50 +0900

    ボタンを押すとwebカメラが機能するようにしました

--- Speech Minutes 2020/Assets/Scripts/WebCameraTest.cs
+++ Speech Minutes 2020/Assets/Scripts/WebCameraTest.cs
@@ -2,17 +2,30 @@
 using UnityEngine.UI;
 
 public class WebCameraTest : MonoBehaviour {
+    public GameObject CameraPanel;
+    bool cameraswitch = false;
 
     public RawImage rawImage;
 
     WebCamTexture webCamTexture;
 
-    void Start () {
-        webCamTexture = new WebCamTexture();
-        rawImage.texture = webCamTexture;
-        webCamTexture.Play();
-	}
-	
-	void Update (){
-	}
+    public void OnClick()
+    {
+        cameraswitch = !cameraswitch;
+        if (cameraswitch)
+        {
+            CameraPanel.SetActive(true);
+            webCamTexture = new WebCamTexture();
+            rawImage.texture = webCamTexture;
+            webCamTexture.Play();
+            Debug.Log("押された!"); // ログを出力
+        }
+        else
+        {
+            CameraPanel.SetActive(false);
+            webCamTexture.Stop();
+        }
+    }
+
+
 }

b8a8bddcbaf293990baf9eb08890213226cf0a66
Okouchi <cstk18015@g.nihon-u.ac.jp>
2021-09-10 16:01:54 +0900

    カメラを移す

---
+++ Speech Minutes 2020/Assets/Scripts/WebCameraTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCameraTest : MonoBehaviour {
+
+    public RawImage rawImage;
+
+    WebCamTexture webCamTexture;
+
+    void Start () {
+        webCamTexture = new WebCamTexture();
+        rawImage.texture = webCamTexture;
+        webCamTexture.Play();
+	}
+	
+	void Update (){
+	}
+}

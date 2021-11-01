c6c127d5fa991c1f6d6dcd932c6e7311b5a5b890
andoureo <65376120+andoureo@users.noreply.github.com>
2021-11-02 05:08:07 +0900

    Recアイコン表現
    Recボタンの点滅とかフェードアウトを試してみました。アセット使ってます。そのせいでMOMプロジェクトが重くなるとかがあればやめます。

--- Speech Minutes 2020/Assets/Scripts/RecButtonChange.cs
+++ Speech Minutes 2020/Assets/Scripts/RecButtonChange.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class RecButtonChange : MonoBehaviour
 {
     public GameObject RecStartButton;
     public GameObject RecStopButton;
+    public Image RecStopImage;
+    public Image RecStartImage;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +26,26 @@ public class RecButtonChange : MonoBehaviour
 
     public void StartRecButtonOnClick()
     {
-            RecStartButton.SetActive(false);
-            RecStopButton.SetActive(true);
+         Transform t = RecStartButton.GetComponent<Transform>();
+        t.DOScale(new Vector3(2f, 2f, 2f), 0.3f);
+        RecStartImage.DOFade(0f, 0.4f);
+        t.DOScale(new Vector3(1.0f, 1.0f, 1.0f), 0f)
+        .SetDelay(0.3f)
+        .OnComplete(() => RecStartButton.SetActive(false));
+        RecStopButton.SetActive(true);
+
+        RecStopImage.DOFade(0.55f, 0.5f)
+        .SetLoops(10, LoopType.Yoyo)
+        .SetDelay(0.6f)
+        .OnComplete(() => RecStopImage.DOFade(1.0f, 2f));
     }
 
     public void StopRecButtonOnClick()
     {
-            RecStopButton.SetActive(false);
-            RecStartButton.SetActive(true);
+        RecStopButton.SetActive(false);
+        RecStartButton.SetActive(true);
+        RecStartImage.DOFade(1f, 0.5f)
+        .SetDelay(0.2f);
     }
-
+  
 }

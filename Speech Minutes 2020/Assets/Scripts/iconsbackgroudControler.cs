79d922ab24cbac8c7bb3177761f279b45c741a1b
andoureo <65376120+andoureo@users.noreply.github.com>
2021-11-09 03:10:42 +0900

    Logパネルを押すと最前面に表示されるようにした
    ログボタンに加え、Logパネル自身を押しても最前面に出るようにしました。また、クローズボタンの❌を画像に変更しました。また、中央下部のアイコンを一定時間触れていない時、消えるようにしました。

---
+++ Speech Minutes 2020/Assets/Scripts/iconsbackgroudControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class iconsbackgroudControler : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public RectTransform CloseButton;
+
+    public void Entericonsbackgroud()
+    {
+        canvasGroup.DOKill();
+        //Debug.Log("Enter");
+        canvasGroup.DOFade(1f, 0.3f);
+    }
+
+    public void Exiticonsbackgroud()
+    {
+        canvasGroup.DOKill();
+        //Debug.Log("Exit");
+        canvasGroup.DOFade(0f, 3f)
+            .SetDelay(2f)
+            .SetEase(Ease.InQuad);
+    }
+}

c2d536641580acac0f0d65d39331e169d9f1b130
andoureo <65376120+andoureo@users.noreply.github.com>
2021-11-27 09:57:09 +0900

    ログパネルの自動スクロールの改善
    ログパネルの自動スクロールがちゃんと一番下に移動するようにしました

--- Speech Minutes 2020/Assets/Watson/Examples/ExampleStreaming.cs
+++ Speech Minutes 2020/Assets/Watson/Examples/ExampleStreaming.cs
@@ -139,9 +139,19 @@ namespace IBM.Watsson.Examples
             {
                 LogText[push].GetComponent<Text>().text += Environment.NewLine;
                 LogText[push].GetComponent<Text>().text += logtime +"," + name + "," + word;
-                ScrollRect[push].verticalNormalizedPosition = 0.0f;
+
+                StartCoroutine(ForceScrollDown(push));
                 //Debug.Log("ScrollPositionIsMoved");
             }
+
+        }
+
+        IEnumerator ForceScrollDown(int push)
+        {
+
+            // 1フレーム待たないと完全に実行されない
+            yield return new WaitForEndOfFrame();
+            ScrollRect[push].verticalNormalizedPosition = 0.0f;
 
         }
 
@@ -382,57 +392,48 @@ namespace IBM.Watsson.Examples
                 case 0:
                     LogDataFilePath = @"/LogDatas/LogData0.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(189, 193, 74, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
 
                     break;
                 case 1:
                     LogDataFilePath = @"/LogDatas/LogData1.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(195, 160, 65, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 case 2:
                     LogDataFilePath = @"/LogDatas/LogData2.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(207, 89, 81, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 case 3:
                     LogDataFilePath = @"/LogDatas/LogData3.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(207, 75, 200, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 case 4:
                     LogDataFilePath = @"/LogDatas/LogData4.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(144, 82, 204, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 case 5:
                     LogDataFilePath = @"/LogDatas/LogData5.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(74, 87, 202, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 case 6:
                     LogDataFilePath = @"/LogDatas/LogData6.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(63, 197, 212, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 case 7:
                     LogDataFilePath = @"/LogDatas/LogData7.txt";
                     filePath = Application.dataPath + LogDataFilePath;
-                    text.text = "現在の話題 "+Button[number].GetComponentInChildren<Text>().text;
-                    text.color = new Color32(62, 207, 69, 255);
+                    text.text = Button[number].GetComponentInChildren<Text>().text + "を選択中";
                     break;
                 default:
                     LogDataFilePath = @"/LogDatas/LogData.txt";
                     filePath = Application.dataPath + LogDataFilePath;
                     text.text = "話題未選択";
-                    text.color = new Color32(0, 0, 0, 255);
                     break;
             }
         }

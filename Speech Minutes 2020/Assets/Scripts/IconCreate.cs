27497a0b2a8485122e0e7f8817863a8d31910ab4
muratakazuma <75768071+Noa-blank@users.noreply.github.com>
2022-01-22 21:44:46 +0900

    ユーザーアイコンのテクスチャを変更

--- Speech Minutes 2020/Assets/Scripts/IconCreate.cs
+++ Speech Minutes 2020/Assets/Scripts/IconCreate.cs
@@ -36,7 +36,38 @@ public class IconCreate : MonobitEngine.MonoBehaviour
     {
         script = GameObject.Find("MUN").GetComponent<MainSecneMUNScript>();
         UserID = script.Iconid;
-        Debug.Log("dicision:" + UserID);
+        switch (UserID%9)
+        {
+            case 0:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon0");
+                break;
+            case 1:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon1");
+                break;
+            case 2:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon2");
+                break;
+            case 3:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon3");
+                break;
+            case 4:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon4");
+                break;
+            case 5:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon5");
+                break;
+            case 6:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon6");
+                break;
+            case 7:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon7");
+                break;
+            case 8:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon8");
+                break;
+            default:
+                break;
+        }
         UserName = script.Iconname;
         IconName.GetComponent<Text>().text = UserName;
         IconInitial.GetComponent<Text>().text = UserName.Substring(0, 1);
@@ -71,6 +102,38 @@ public class IconCreate : MonobitEngine.MonoBehaviour
     public void IconSync(int id, string name)
     {
         UserID = id;
+        switch (UserID % 9)
+        {
+            case 0:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon0");
+                break;
+            case 1:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon1");
+                break;
+            case 2:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon2");
+                break;
+            case 3:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon3");
+                break;
+            case 4:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon4");
+                break;
+            case 5:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon5");
+                break;
+            case 6:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon6");
+                break;
+            case 7:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon7");
+                break;
+            case 8:
+                IconImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("textures/Icon8");
+                break;
+            default:
+                break;
+        }
         UserName = name;
         IconName.GetComponent<Text>().text = UserName;
         IconInitial.GetComponent<Text>().text = UserName.Substring(0, 1);
